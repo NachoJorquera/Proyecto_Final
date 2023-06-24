1a89f0a6342a0165e672f6119a42cd44aea37b60
NachoJorquera <111363732+NachoJorquera@users.noreply.github.com>
2023-06-23 22:15:21 -0400

    Update ejem05_procedural.py

--- ejem05_procedural.py
+++ ejem05_procedural.py
@@ -1,15 +1,24 @@
-import requests
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.common.by import By
+from selenium.webdriver.chrome.options import Options
 from bs4 import BeautifulSoup
 
 def obtener_datos(url):
-    response = requests.get(url)
-    soup = BeautifulSoup(response.text, 'html.parser')
+    options = Options()
+    options.add_argument("--headless")  # Ejecutar Chrome en modo headless, sin interfaz gráfica
+    service = Service('path/to/chromedriver')  # Reemplaza 'path/to/chromedriver' por la ubicación de tu chromedriver
+    driver = webdriver.Chrome(service=service, options=options)
+    driver.get(url)
+    soup = BeautifulSoup(driver.page_source, 'html.parser')
     productos = []
 
     for producto in soup.find_all('div', class_='product'):
         nombre = producto.find('h4', class_='product-name').text.strip()
         precio = producto.find('div', class_='price').text.strip()
         productos.append({'nombre': nombre, 'precio': precio})
+
+    driver.quit()
 
     return productos
 
