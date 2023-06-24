27a65b84bef23a598a796beee34f498a61c13ea2
NachoJorquera <111363732+NachoJorquera@users.noreply.github.com>
2023-06-24 18:27:42 -0400

    Update banco_central.py

--- banco_central.py
+++ banco_central.py
@@ -5,8 +5,7 @@ from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.common.by import By
 
-# MAIN
-if (__name__ == "__main__"):
+def obtener_uf():
     # Driver y carga de página
     chrome_options = Options()
     chrome_options.add_argument('--no-sandbox')
@@ -17,24 +16,12 @@ if (__name__ == "__main__"):
     #
     # Obtener UF por content Selenium + lxml BeautifulSoup
     #
-    #weUFContent = driver.find_element(By.XPATH, '/html/body/div[1]/div[1]/section/div/div[2]/div/div/div[1]/section/div/div[2]/div/div/div/div/div[1]/div/div')
     weUFContent = driver.find_element(By.XPATH, '/html/body/div[1]/section/div/div[2]/div/div/div[1]/section/div/div[2]/div/div/div/div/div[1]/div/div')
     htmlData = weUFContent.get_attribute('innerHTML')
     lxmlData = BeautifulSoup(htmlData, 'lxml')
 
-    # Generar html
-    '''
-    fOutputHtml = open ('bcentral_tc.html','w')
-    fOutputHtml.write(lxmlData.prettify())
-    fOutputHtml.close()
-    '''
-
     listTC = lxmlData.find_all('p', class_='basic-text fs-2 f-opensans-bold text-center c-blue-nb-2')
-    #print("Contenedor tipos de cambio:")
-    #for sTC in listTC:
-    #    print('{}'.format(sTC.string)) #.replace('\n', '').replace('\t', ''))))
-    #print("")
-    
+
     # Captura UF
     sUF = listTC[0].string
     sUF = sUF.replace('$', '').replace('.', '').replace(',', '.')
@@ -51,9 +38,11 @@ if (__name__ == "__main__"):
             escritor_csv.writerow(['Fecha', 'Valor UF'])
         escritor_csv.writerow(datos_uf)
 
-    print("\n")
-    print("Información almacenada en parametros.csv")
-   
     # Cierre del driver
-    #driver.close()
     driver.quit()
+
+    return "Información almacenada en parametros.csv"
+
+
+if __name__ == "__main__":
+    obtener_uf()
